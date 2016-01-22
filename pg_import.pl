7a82e79a9771cc0c2c1e6dad14677f6433227877
brianhuey <sbhuey@gmail.com>
2016-01-22 14:12:47 -0800

    statcast_table

--- pg_import.pl
+++ pg_import.pl
@@ -167,7 +167,7 @@ sub statcast_table {
                 $event_num = $1;
             }
             my ($distance, $speed, $angle) = description($item->{data}->{description});
-            if (($distance = 'null') and ($speed = 'null') and ($angle = 'null') {
+            if (($distance = 'null') and ($speed = 'null') and ($angle = 'null')) {
                 # If no statcast data, don't submit
             } else {
                 $sc_query = 'INSERT INTO statcast (game_id, event_num, distance, speed, angle) '
